29c02f53c1bfceda81daa1ab28bb8809f1bfcbf5
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-25 00:03:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import Header from "./components/Header";
+import AboutPage from "./components/pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+import Post from "./components/Post";
+import { FeedbackProvider } from "./context/FeedbackContext";
+
+function App() {
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            />
+            <Route exact path="/about"
+              element={<AboutPage />
+              } 
+            />
+            <Route exact path="/post/:id/:name"
+              element={<Post />
+              } 
+              />
+            
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
+}
+
+export default App;
